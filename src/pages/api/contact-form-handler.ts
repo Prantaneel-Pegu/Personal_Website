0293a7d86e28330721347563d8c463256d84524e
Prantaneel Pegu <159791377+Prantaneel-Pegu@users.noreply.github.com>
2025-08-04 18:30:44 +0530

    contact form functionality added

--- src/pages/api/contact-form-handler.ts
+++ src/pages/api/contact-form-handler.ts
@@ -1,16 +1,38 @@
 import type { APIRoute } from 'astro';
+import { Resend } from 'resend';
 
 export const prerender = false;
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const POST: APIRoute = async ({ request }) => {
     try {
         const formData = await request.formData();
 
-        const name = formData.get('name');
-        const email = formData.get('email');
-        const message = formData.get('message');
+        const name = formData.get('name') || '';
+        const email = formData.get('email') || '';
+        const message = (formData.get('message') as string) || '';
 
-        console.log({ name, email, message });
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { data, error } = await resend.emails.send({
+            from: 'api@prantaneelpegu.com',
+            to: 'contact@prantaneelpegu.com',
+            subject: 'New Contact Form Submission',
+            html: `
+                        <strong>Name:</strong> ${name}<br>
+                        <strong>Email:</strong> ${email}<br>
+                        <strong>Message:</strong><br> ${message.replace(/\n/g, '<br>')}
+                    `,
+            headers: {
+                'X-Priority': '1 (Highest)',
+                'X-MSMail-Priority': 'High',
+                Importance: 'High',
+            },
+        });
+
+        if (error) {
+            console.error('Form processing error:', error);
+            return Response.json('Error processing form.', { status: 500 });
+        }
 
         return new Response('Form submitted successfully.', { status: 200 });
     } catch (error) {
